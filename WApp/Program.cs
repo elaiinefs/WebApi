257454ee7a5cfe0b64f63ce36a8517167c22e900
elaiinefs <elaiineferrer@gmail.com>
2019-06-25 22:26:35 -0400

    removing server headers from api response

--- WApp/Program.cs
+++ WApp/Program.cs
@@ -19,6 +19,7 @@ namespace WApp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .UseKestrel(options => options.AddServerHeader = false)
                 .UseStartup<Startup>();
     }
 }
